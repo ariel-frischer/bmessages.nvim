7072a6eab3d783af2d2f8ac5a1230beefe978585
Ariel Frischer <arielfrischer@gmail.com>
2023-12-17 12:05:22 -0800

    Improve readme, docs, readability of lua code and fix some bugs in the way local options override global ones.

--- lua/bmessages.lua
+++ lua/bmessages.lua
@@ -2,61 +2,59 @@
 -- email: arielfrischer@gmail.com
 
 local M = {}
+M.current_split_type = nil
 
 local function with_defaults(options)
   options = options or {}
 
-  return {
-    timer_interval = {
-      description = "Time in milliseconds between each update of the messages buffer.",
-      default = options.timer_interval or 1000
-    },
-    split_type = {
-      description = "Default split type for the messages buffer (vsplit or split).",
-      default = options.split_type or "vsplit"
-    },
-    buffer_name = {
-      description = "Name of the messages buffer.",
-      default = options.buffer_name or "messages_buffer"
-    },
-    split_size_vsplit = {
-      description = "Size of the vertical split when opening the messages buffer. Check :h resize",
-      default = options.split_size_vsplit or nil
-    },
-    split_size_split = {
-      description = "Size of the horizontal split when opening the messages buffer. Check :h resize",
-      default = options.split_size_split or nil
-    },
-    autoscroll = {
-      description = "Automatically scroll to the latest message in the buffer.",
-      default = options.autoscroll ~= nil and options.autoscroll or true
-    },
-    use_timer = {
-      description =
-      "Use a timer to auto-update the messages buffer. If this is false the buffer can be modified, but will not auto-update.",
-      default = options.use_timer ~= nil and options.use_timer or true
-    },
+  local defaults = {
+    timer_interval = 1000,
+    split_type = "vsplit",
+    buffer_name = "bmessages_buffer",
+    split_size_vsplit = nil,
+    split_size_split = nil,
+    autoscroll = true,
+    use_timer = true,
+    disable_create_user_commands = false,
   }
+
+  for key, default_value in pairs(defaults) do
+    if options[key] == nil then
+      options[key] = default_value
+    end
+  end
+
+  return options
 end
 
-local function update_messages_buffer()
-  local new_messages = vim.api.nvim_exec("messages", true)
-  if new_messages == "" then return nil end
+local function is_bmessages_buffer_open(options)
+  local bufnr = vim.fn.bufnr(options.buffer_name)
+  return vim.api.nvim_buf_is_valid(bufnr) and vim.api.nvim_buf_is_loaded(bufnr)
+end
 
-  -- print('updated!')
+local function update_messages_buffer(options)
+  return function()
+    print('updated')
+    local new_messages = vim.api.nvim_exec("messages", true)
+    if new_messages == "" then return end
 
-  local bufnr = vim.fn.bufnr(M.options.buffer_name.default)
-  if not vim.api.nvim_buf_is_valid(bufnr) then return end
-  local lines = vim.split(new_messages, "\n")
+    local bufnr = vim.fn.bufnr(options.buffer_name)
+    if not vim.api.nvim_buf_is_valid(bufnr) then return end
 
-  local current_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
+    local lines = vim.split(new_messages, "\n")
+    local current_lines = vim.api.nvim_buf_get_lines(bufnr, 0, -1, false)
 
-  if not vim.deep_equal(current_lines, lines) then
+    if vim.deep_equal(current_lines, lines) then return end
+
     vim.api.nvim_buf_set_option(bufnr, 'modifiable', true)
     vim.api.nvim_buf_set_lines(bufnr, 0, -1, false, lines)
-    vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
+    if options.use_timer then
+      vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
+    end
 
-    if M.options.autoscroll.default and vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) ~= M.options.buffer_name.default then
+    -- __AUTO_GENERATED_PRINT_VAR_START__
+    print("update_messages_buffer#function#if autoscroll:", vim.inspect(options.autoscroll)) -- __AUTO_GENERATED_PRINT_VAR_END__
+    if options.autoscroll and vim.api.nvim_buf_get_name(vim.api.nvim_get_current_buf()) ~= options.buffer_name then
       local winnr = vim.fn.bufwinnr(bufnr)
       if winnr ~= -1 then
         local winid = vim.fn.win_getid(winnr)
@@ -66,40 +64,78 @@ local function update_messages_buffer()
   end
 end
 
-local function create_messages_buffer(options)
-  local cmd = options.split_type.default
-
-  if cmd == "vsplit" and options.split_size_vsplit.default ~= nil then
-    cmd = cmd .. " | vertical resize " .. options.split_size_vsplit.default
-  elseif cmd == "split" and options.split_size_split.default ~= nil then
-    cmd = cmd .. " | resize " .. options.split_size_split.default
+local function merge_options(defaults, new_options)
+  if not new_options then
+    return defaults
   end
+  return vim.tbl_deep_extend("force", {}, defaults, new_options)
+end
 
-  vim.cmd(cmd .. " | enew")
-
+local function create_raw_buffer(options)
   local bufnr = vim.api.nvim_get_current_buf()
-  vim.api.nvim_buf_set_name(bufnr, options.buffer_name.default)
+  vim.api.nvim_buf_set_name(bufnr, options.buffer_name)
   vim.api.nvim_buf_set_option(bufnr, 'buftype', 'nofile')
   vim.api.nvim_buf_set_option(bufnr, 'bufhidden', 'wipe')
   vim.api.nvim_buf_set_option(bufnr, 'bl', false)
   vim.api.nvim_buf_set_option(bufnr, 'swapfile', false)
+end
 
-  update_messages_buffer()
-  if not options.use_timer.default then
+local function run_vim_cmd(options)
+  local cmd = options.split_type
+
+  if cmd == "vsplit" and options.split_size_vsplit ~= nil then
+    cmd = cmd .. " | vertical resize " .. options.split_size_vsplit
+  elseif cmd == "split" and options.split_size_split ~= nil then
+    cmd = cmd .. " | resize " .. options.split_size_split
+  end
+
+  vim.cmd(cmd .. " | enew")
+end
+
+
+local function create_messages_buffer(new_options)
+  local options = merge_options(M.options, new_options)
+  -- __AUTO_GENERATED_PRINT_VAR_START__
+  vim.print("create_messages_buffer options:", options) -- __AUTO_GENERATED_PRINT_VAR_END__
+
+  if is_bmessages_buffer_open(options) then
+    if M.current_split_type == options.split_type then
+      vim.api.nvim_buf_delete(vim.fn.bufnr(options.buffer_name), { force = true })
+      return nil
+    else
+      vim.api.nvim_buf_delete(vim.fn.bufnr(options.buffer_name), { force = true })
+    end
+  end
+
+  M.current_split_type = options.split_type
+
+  run_vim_cmd(options)
+  create_raw_buffer(options)
+
+  local update_fn = update_messages_buffer(options)
+  update_fn()
+
+  local timer = vim.loop.new_timer()
+  local function close_timer()
+    timer:stop()
+    timer:close()
+    timer = nil
+  end
+
+  if not options.use_timer then
+    if timer then close_timer() end
     return nil
   end
 
-  vim.api.nvim_buf_set_option(bufnr, 'modifiable', false)
-  local timer = vim.loop.new_timer()
-  timer:start(options.timer_interval.default, options.timer_interval.default, vim.schedule_wrap(update_messages_buffer))
+  if options.use_timer then
+    timer:start(options.timer_interval, options.timer_interval, vim.schedule_wrap(update_fn))
+  end
 
   vim.api.nvim_create_autocmd({ "BufWipeout", "BufDelete" }, {
-    pattern = options.buffer_name.default,
+    pattern = options.buffer_name,
     callback = function()
       if timer then
-        timer:stop()
-        timer:close()
-        timer = nil
+        close_timer()
       end
     end,
   })
@@ -109,22 +145,27 @@ end
 function M.setup(options)
   M.options = with_defaults(options)
 
+  if M.options.disable_create_user_commands then return end
+
   vim.api.nvim_create_user_command('Bmessages', function()
     create_messages_buffer(M.options)
   end, {})
 
   vim.api.nvim_create_user_command('Bmessagesvs', function()
-    create_messages_buffer(vim.tbl_deep_extend("force", {}, M.options, { split_type = { default = "vsplit" } }))
+    create_messages_buffer(vim.tbl_deep_extend("force", {}, M.options, { split_type = "vsplit" }))
   end, {})
 
   vim.api.nvim_create_user_command('Bmessagessp', function()
-    create_messages_buffer(vim.tbl_deep_extend("force", {}, M.options, { split_type = { default = "split" } }))
+    create_messages_buffer(vim.tbl_deep_extend("force", {}, M.options, { split_type = "split" }))
   end, {})
 end
 
--- function M.is_configured()
---   return M.options ~= nil
--- end
+function M.is_configured()
+  return M.options ~= nil
+end
+
+-- Users can call this function directly in lua with: require("bmesssages").toggle()
+M.toggle = create_messages_buffer
 
 M.options = nil
 return M
